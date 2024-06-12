59f9dcf6019db8037db118ec6d01810cc6c0a43c
José Esmerino <jose.esmerino@gmail.com>
2024-06-12 00:04:11 -0300

    Set lints

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/autorun"
 
 module ActiveSupport
   class TestCase
+    include FactoryBot::Syntax::Methods
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
